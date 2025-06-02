2b8937ce0ccae6eff896b125ac907258a5690f3b
Beletatti <kaiquebeletatti@gmail.com>
2025-06-02 11:34:45 -0300

    métodos no java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //expressões no código abaixo: myVariable = 50 e fatura = 50.
+        int myVariable = 20;
+        int fatura = 400;
+
+        // expressões no código abaixo: (myVariable > 49), (fatura > 499) e a expressão inteira ((myVariable > 49) && (fatura > 499))
+        if ((myVariable > 49) && (fatura > 499)) {
+            System.out.println("Ficaramos bem esse mês");
+        } else {
+            System.out.println("F total, sobra nada pro beta");
+        }
+
+    }
+}
