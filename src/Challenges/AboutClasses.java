6e019f8c0e142974bad5ba5b9a4220eb40fc528a
Beletatti <kaiquebeletatti@gmail.com>
2025-06-06 13:33:41 -0300

    exercitando loops do-while e try-catch

--- src/Challenges/AboutClasses.java
+++ src/Challenges/AboutClasses.java
@@ -24,22 +24,22 @@ public class AboutClasses {
 
         // interage com o terminal
         try {
-            System.out.println(getInputFromConsole(currentYear));
+            System.out.println(getInputFromScanner(currentYear));
         } catch (NullPointerException e) {
             System.out.println(getInputFromScanner(currentYear));
         }
     }
 
-    public static String getInputFromConsole(int currentYear){
-
-        String name = System.console().readLine("What's your name? ");
-        System.out.println("Hello " + name + "!");
-        String dateOfBirth = System.console().readLine("What year you born? ");
-        int age = currentYear - Integer.parseInt(dateOfBirth);
-
-        return "So you are " + age + " years old";
-
-    }
+//    public static String getInputFromConsole(int currentYear){
+//
+//        String name = System.console().readLine("What's your name? ");
+//        System.out.println("Hello " + name + "!");
+//        String dateOfBirth = System.console().readLine("What year you born? ");
+//        int age = currentYear - Integer.parseInt(dateOfBirth);
+//
+//        return "So you are " + age + " years old";
+//
+//    }
 
     public static String getInputFromScanner(int currentYear){
         Scanner scanner = new Scanner(System.in);
@@ -50,11 +50,33 @@ public class AboutClasses {
         System.out.println("Hello " + name + "!");
 
         System.out.print("What year were you born? ");
-        String dateOfBirth = scanner.nextLine();
-        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        boolean validDOB = false;
+        int age = 0;
+
+        do{
+            System.out.println("Enter a year of birth >= " + (currentYear - 125) + " and <= " + currentYear);
+            try{
+                age = checkData(currentYear, scanner.nextLine());
+                validDOB = age < 0 ? false : true;
+            } catch (NumberFormatException badUserData){
+                System.out.println("Invalid input. Please enter a number.");
+            }
+        } while (!validDOB);
 
         return "So you are " + age + " years old";
 
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth){
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - 125;
+
+        if ((dob < minimumYear) || (dob > currentYear)){
+            return -1;
+        }
+
+        return (currentYear - dob);
     }
 }
 
