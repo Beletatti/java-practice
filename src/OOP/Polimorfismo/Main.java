d51f475b5e64ff9b16bcd41549e90e4271aa3b5b
Beletatti <kaiquebeletatti@gmail.com>
2025-06-16 17:06:18 -0300

    praticando mais polimorfismo

---
+++ src/OOP/Polimorfismo/Main.java
@@ -0,0 +1,9 @@
+package OOP.Polimorfismo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Movie theMovie = new Adventure("The Godfather");
+        theMovie.watchMovie();
+    }
+}
