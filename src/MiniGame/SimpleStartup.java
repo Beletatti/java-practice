fa2140760381c13a33787c0727a8d9ba52cdf39f
Beletatti <kaiquebeletatti@gmail.com>
2026-03-25 21:43:43 -0300

    ?

---
+++ src/MiniGame/SimpleStartup.java
@@ -0,0 +1,15 @@
+package MiniGame;
+
+public class SimpleStartup {
+
+    private int[] locationCells;
+    private int numOfHits;
+
+    public String checkYourself(int userGuess){
+        return "testando";
+    }
+
+    public void setLocationCells(int[] cellLocations){
+
+    }
+}
